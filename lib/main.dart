fd99df6128499dd7c8423472246c4090bda7c388
Abhishek Shah <amitkumarshah499@gmail.com>
2023-03-30 14:32:35 +0530

    local json file storage

--- lib/main.dart
+++ lib/main.dart
@@ -1,110 +1,97 @@
 import 'package:flutter/material.dart';
-import 'package:path_provider/path_provider.dart';
 import 'dart:convert';
-import 'dart:async';
-import 'dart:io';
+import 'package:flutter/services.dart';
 
 
 void main(){
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
 
   @override
   Widget build(BuildContext context){
-    return const MaterialApp(
-      title: "Read and Write Storage",
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
+    return MaterialApp(
+      title: 'Local Json File ',
+      theme: ThemeData(
+        useMaterial3: true,
+        primarySwatch: Colors.lightBlue
+      ),
+
+      home: const HomePage(),
     );
   }
 }
 
-
-class HomePage extends StatefulWidget{
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
-
   @override
-  State<StatefulWidget> createState(){
-
+  State<StatefulWidget> createState() {
     return _HomePageState();
   }
 }
 
+
 class _HomePageState extends State<HomePage>{
- String? _content;
 
- Future<String> _getDirPath() async{
-   final dir = await getApplicationDocumentsDirectory();
-   return dir.path;
- }
+  List _items = [];
 
+  // Fetch content from the json file
+  Future<void> readJson() async {
+    final String response = await rootBundle.loadString('assets/sample.json');
+    final data = await json.decode(response);
+    setState(() {
+      _items = data["items"];
+    });
+  }
 
- Future<void> _readData() async{
-   final dirPath = await _getDirPath();
-   final myFile = File('$dirPath/data.txt');
-   final data = await myFile.readAsString(encoding: utf8);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          'Kindacode.com',
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(25),
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: readJson,
+              child: const Text('Load Data'),
+            ),
 
-   setState(() {
-     _content = data;
-   });
- }
+            // Display the data loaded from sample.json
+            _items.isNotEmpty
+                ? Expanded(
+              child: ListView.builder(
+                itemCount: _items.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    key: ValueKey(_items[index]["id"]),
+                    margin: const EdgeInsets.all(10),
+                    color: Colors.amber.shade100,
+                    child: ListTile(
+                      leading: Text(_items[index]["id"]),
+                      title: Text(_items[index]["name"]),
+                      subtitle: Text(_items[index]["description"]),
+                    ),
+                  );
+                },
+              ),
+            )
+                : Container()
+          ],
+        ),
+      ),
+    );
+  }
 
- final _textController = TextEditingController();
-
- Future<void> _writeData() async{
-   final _dirPath = await _getDirPath();
-   final _myFile =  File('$_dirPath/data.txt');
-
-   await _myFile.writeAsString(_textController.text);
-   _textController.clear();
- }
-
-
-
- @override
-  Widget build(BuildContext context){
-   return Scaffold(
-     appBar: AppBar(title: const Text('Read and Write Dynamic'),),
-
-     body: Padding(
-       padding: const EdgeInsets.all(20),
-       child: Column(
-         children: [
-           TextField(
-             controller: _textController,
-             decoration: const InputDecoration(
-               labelText: 'Enter Your Name:'
-             ),
-           ),
-           ElevatedButton(onPressed: _writeData, child: const Text('Save to file')),
-           const SizedBox(
-             height: 100,
-           ),
-
-           Text(_content ?? 'press the button to load the data from file', style: const TextStyle(fontSize: 24,color:Colors.deepOrange),),
-           ElevatedButton(onPressed: _readData, child: const Text('Read stored file'))
-         ],
-       ),
-     ),
-   );
- }
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
